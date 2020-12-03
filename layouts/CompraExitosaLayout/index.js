3616443564e32ed4463f17d4d30d62904c404858
ScriptKev <script.kev@gmail.com>
2020-12-03 13:07:29 -0500

    installing library test with enzyme and adding emoji's png to 'compra exitosa' layout

--- layouts/CompraExitosaLayout/index.js
+++ layouts/CompraExitosaLayout/index.js
@@ -1,9 +1,16 @@
+import Image from 'next/image'
 import { CompraExitosaLayoutStyled } from './styled'
 
 export default function CompraExitosaLayout() {
   return (
     <CompraExitosaLayoutStyled>
-      <h5>Orden exitosa 😋🥳</h5>
+      <h5 style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        Orden exitosa
+        <div style={{ display: 'flex', margin: '20px 0' }}>
+          <Image style={{ marginLeft: '10px' }} src='/img/emoji-savouring-face.png' width={90} height={90} alt='emoji-savouring' />
+          <Image src='/img/emojy-party-face.png' width={90} height={90} alt='emoji-party' />
+        </div>
+      </h5>
       <p>Estaremos recibiendo su pedido via whatsapp</p>
       <p>Regresando al inicio...</p>
     </CompraExitosaLayoutStyled>
