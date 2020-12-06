367439e1deaeb4e972e4c66bb5897eaa52ae025c
ScriptKev <script.kev@gmail.com>
2020-12-06 08:19:55 -0500

    remove comment

--- components/MainProduct/index.js
+++ components/MainProduct/index.js
@@ -40,12 +40,6 @@ export default function MainProduct({ product }) {
       <ContainerStyled>
         <FlexStyled>
           <SelectedProductStyled onClick={() => ProductSelected(product)}>
-            {/* <Image
-              src={product.img}
-              alt="Picture of the author"
-              width={500}
-              height={500}
-            /> */}
             <MainProductImageStyled src={product.img} alt={product.title} />
           </SelectedProductStyled>
 
